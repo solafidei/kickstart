725b169b3e451bce9800535d186a7fc72fd0ec6d
solafidei <souls1@live.co.za>
2023-05-02 22:28:57 +0200

    chore: support users that don't have metamask

--- ethereum/factory.ts
+++ ethereum/factory.ts
@@ -2,15 +2,13 @@ import { Contract } from "web3-eth-contract";
 import getWeb3 from "./getWeb3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
-let instance: Contract | null = null;
+let instance: Contract;
 
 const web3Instance = getWeb3();
 
-if (web3Instance !== null) {
-  instance = new web3Instance.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    "0x92090A270199Be1ed1941796c5973A81325CEAAc"
-  );
-}
+instance = new web3Instance.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  "0x92090A270199Be1ed1941796c5973A81325CEAAc"
+);
 
 export default instance;
--- ethereum/getWeb3.ts
+++ ethereum/getWeb3.ts
@@ -4,9 +4,12 @@ const getWeb3 = () => {
   if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
     window.ethereum.request({ method: "eth_requestAccounts" });
     return new Web3(window.ethereum);
+  } else {
+    const provider = new Web3.providers.HttpProvider(
+      process.env.INFURA_URL as string
+    );
+    return new Web3(provider);
   }
-
-  return null;
 };
 
 export default getWeb3;
