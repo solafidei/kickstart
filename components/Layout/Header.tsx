18696eb7166cce231fd7524eb608f8c592a3a7fa
solafidei <souls1@live.co.za>
2023-05-08 18:56:31 +0200

    frontend: add buttons to header

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,5 +1,13 @@
-import { AppBar, Container, Toolbar, Typography, Button } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
 import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
@@ -11,18 +19,30 @@ const Header: React.FC = () => {
   return (
     <AppBar position="static">
       <Container>
-        <Toolbar disableGutters>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            CrowdCoin
-          </Typography>
-          <Button
-            color="inherit"
-            aria-controls="nav-menu"
-            aria-haspopup="true"
-            onClick={(e) => setAnchorElNav(e.currentTarget)}
-          >
-            Campaigns
-          </Button>
+        <Toolbar>
+          <Link href="/">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Box
+                sx={{
+                  textDecoration: "none",
+                  color: "inherit",
+                  cursor: "pointer",
+                }}
+              >
+                CrowdCoin
+              </Box>
+            </Typography>
+          </Link>
+          <Link href="/">
+            <Button color="inherit" variant="outlined">
+              Campaigns
+            </Button>
+          </Link>
+          <Link href="/campaigns/new">
+            <Button color="inherit" variant="outlined">
+              +
+            </Button>
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
