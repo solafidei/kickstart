31c725de862da87e5f0e9f15f2e2503e7049f405
solafidei <souls1@live.co.za>
2023-05-08 18:57:27 +0200

    ethereum: getsummary and getRequestsCount in campaign contract

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.17;
 
-contract CampaignFactory  {
+contract CampaignFactory {
     address[] public deployedCampaigns;
 
     function createCampaign(uint minimum) public {
         address newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
-    } 
+    }
 
     function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
@@ -20,7 +20,7 @@ contract Campaign {
         address recipient;
         bool complete;
         uint approvalCount;
-        mapping (address => bool) approvals;    
+        mapping(address => bool) approvals;
     }
 
     address public manager;
@@ -29,7 +29,7 @@ contract Campaign {
     Request[] public requests;
     uint public approversCount;
 
-    modifier restricted(){
+    modifier restricted() {
         require(msg.sender == manager);
         _;
     }
@@ -46,7 +46,11 @@ contract Campaign {
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
+    function createRequest(
+        string description,
+        uint value,
+        address recipient
+    ) public restricted {
         Request memory newRequest = Request({
             description: description,
             value: value,
@@ -59,15 +63,12 @@ contract Campaign {
     }
 
     function approveRequest(uint index) public {
-
         Request storage request = requests[index];
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
 
-
         request.approvals[msg.sender] = true;
         request.approvalCount++;
-
     }
 
     function finalizeRequest(uint index) public restricted {
@@ -79,6 +80,23 @@ contract Campaign {
         request.recipient.transfer(request.value);
 
         request.complete = true;
+    }
 
+    function getSummary()
+        public
+        view
+        returns (uint, uint, uint, uint, address)
+    {
+        return (
+            minimumContribution,
+            this.balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
