3947e6e6f941f15d16ae88239667900053d2f636
solafidei <souls1@live.co.za>
2023-05-08 18:58:27 +0200

    ethereum: add campaign helper

--- ethereum/factory.ts
+++ ethereum/factory.ts
@@ -1,10 +1,9 @@
-import { Contract } from "web3-eth-contract";
 import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x92090A270199Be1ed1941796c5973A81325CEAAc"
+  "0x96DF8FC030Bb7894E5eE9a172259c57106da5f4C"
 );
 
 export default instance;
